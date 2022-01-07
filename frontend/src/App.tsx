53c030ca6c3768938d65c96ccc64af91dccfedc5
Pedro <pedro-gomes17@hotmail.com>
2022-01-07 20:41:58 -0300

    feat: added typescript package and update all components and files to using typescript

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './components/store';
 import Routes from './routes';
-import { Container } from '@material-ui/core/';
+import { Container } from '@material-ui/core';
 import Header from './components/Header';
 
 const App = () => {
