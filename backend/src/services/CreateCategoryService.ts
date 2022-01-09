e2800a66ed3034eb8fdca3b1172421b5b3a76cf5
Pedro <pedro-gomes17@hotmail.com>
2022-01-09 07:28:55 -0300

    feat: created category service

---
+++ backend/src/services/CreateCategoryService.ts
@@ -0,0 +1,31 @@
+import { getCustomRepository } from "typeorm";
+import { CategoryRepository } from "../repository/CategoryRepository";
+
+interface IMessage {
+  name: string;
+}
+
+class CreateCategoryService {
+  async execute({ name }: IMessage) {
+    const categoryRepository = getCustomRepository(CategoryRepository);
+
+    const categoryAlreadyExists = await categoryRepository.findOne({
+      name
+    })
+
+    if (categoryAlreadyExists) {
+      throw new Error("There is already a category with this name")
+    }
+
+    if (!name) {
+      throw new Error("Please, enter a name")
+    }
+    const newCategory = categoryRepository.create({ name })
+
+    await categoryRepository.save(newCategory);
+
+    return newCategory;
+  }
+}
+
+export { CreateCategoryService }
