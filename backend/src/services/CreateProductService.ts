a580f1ecf79fcb3e53c9c6f2777106e85c1eaacb
Pedro <pedro-gomes17@hotmail.com>
2022-01-09 09:53:52 -0300

    feat: create product service

---
+++ backend/src/services/CreateProductService.ts
@@ -0,0 +1,30 @@
+import { getCustomRepository } from "typeorm";
+import { ProductRepository } from "../repository/ProductRepository";
+import { CategoryRepository } from "../repository/CategoryRepository";
+
+interface ICreateProduct {
+  category_id: string;
+  name: string;
+  price: number;
+}
+
+class CreateProductService {
+  async execute({ category_id, name, price }: ICreateProduct) {
+    const productRepository = getCustomRepository(ProductRepository);
+    const categoryRepository = getCustomRepository(CategoryRepository);
+
+    const categoryExists = await categoryRepository.findOne(category_id)
+    
+    if (!categoryExists) {
+      throw new Error("Categories does not exists")
+    }
+
+    const newProduct = productRepository.create({ name, price, category_id })
+
+    await productRepository.save(newProduct);
+
+    return newProduct;
+  }
+}
+
+export { CreateProductService }
