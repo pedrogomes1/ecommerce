938dbc98f25821107edf5e015c60536f0daac34b
Pedro <pedro-gomes17@hotmail.com>
2022-01-09 14:30:47 -0300

    chore: added filter by product name

--- backend/src/controllers/ListProductsController.ts
+++ backend/src/controllers/ListProductsController.ts
@@ -2,15 +2,16 @@ import { Request, Response } from "express";
 import { ListProductsService } from "../services/ListProductsService";
 
 interface IListProductsController {
+    name?: string;
     categories?: string[];
 }
 
 class ListProductsController {
     async handle(request: Request, response: Response){
         const listProductsService = new ListProductsService();
-        const { categories }: IListProductsController = request.query;
+        const { categories, name }: IListProductsController = request.query;
 
-        const allProducts = await listProductsService.execute({ categories });
+        const allProducts = await listProductsService.execute({ categories, name });
         return response.json(allProducts)
     }
 }
--- backend/src/services/ListProductsService.ts
+++ backend/src/services/ListProductsService.ts
@@ -1,17 +1,19 @@
-import { getCustomRepository, In } from "typeorm";
+import { getCustomRepository, In, ILike } from "typeorm";
 import { ProductRepository } from "../repository/ProductRepository";
 
 interface IListProductsService {
+    name: string;
     categories: string[];
 }
 
 class ListProductsService {
-    async execute({ categories }: IListProductsService) {
+    async execute({ categories, name }: IListProductsService) {
         const productsRepository = getCustomRepository(ProductRepository);
-
+         
         const allProducts = await productsRepository.find({
             where: {
                 category_id: In(categories || []),
+                name: ILike(name ? `${name}%` : "%"),
             }
         });
         return allProducts
