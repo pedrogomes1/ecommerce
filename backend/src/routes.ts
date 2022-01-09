06528a59f51267b2554fe0076e8e282f8c62c07a
Pedro <pedro-gomes17@hotmail.com>
2022-01-09 08:24:33 -0300

    feat: added new route to category delete

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,17 +1,20 @@
 import { Router, Request, Response } from "express";
 import { CreateMessageController } from "./controllers/CreateMessageController";
 import { ListMessageController } from "./controllers/ListMessageController";
+import { ListCategoriesController } from "./controllers/ListCategoriesController";
 import { CreateCategoryController } from "./controllers/CreateCategoryController";
 import { UpdateCategoryController } from "./controllers/UpdateCategoryController";
-import { ListCategoriesController } from "./controllers/ListCategoriesController";
+import { DeleteCategoryController } from "./controllers/DeleteCategoryController";
 
 const router = Router();
 
 const createMessageController = new CreateMessageController();
 const listMessageController = new ListMessageController();
+
+const listCategoriesController = new ListCategoriesController();
 const createCategoryController = new CreateCategoryController();
 const updateCategoryController = new UpdateCategoryController();
-const listCategoriesController = new ListCategoriesController();
+const deleteCategoryController = new DeleteCategoryController();
 
 router.get('/', (request: Request, response: Response) => {
     return response.json({message: 'Bem vindo a API Dio Shopping'})
@@ -21,6 +24,7 @@ router.get('/message', listMessageController.handle)
 router.post('/message', createMessageController.handle)
 router.post('/category', createCategoryController.handle)
 router.put('/category/:id', updateCategoryController.handle)
+router.delete('/category/:id', deleteCategoryController.handle)
 router.get('/categories', listCategoriesController.handle)
 
 export { router }
