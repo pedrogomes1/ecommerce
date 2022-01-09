5aaedc6ca4d758ec9e082079c1cdd97e2c6d559a
Pedro <pedro-gomes17@hotmail.com>
2022-01-09 07:33:41 -0300

    chore: added new routes to categories and fix handle function name to listMessageController

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,17 +1,23 @@
 import { Router, Request, Response } from "express";
 import { CreateMessageController } from "./controllers/CreateMessageController";
 import { ListMessageController } from "./controllers/ListMessageController";
+import { CategoryController } from "./controllers/CreateCategoryController";
+import { ListCategoriesController } from "./controllers/ListCategoriesController";
 
 const router = Router();
 
 const createMessageController = new CreateMessageController();
 const listMessageController = new ListMessageController();
+const categoryController = new CategoryController();
+const listCategoriesController = new ListCategoriesController();
 
 router.get('/', (request: Request, response: Response) => {
     return response.json({message: 'Bem vindo a API Dio Shopping'})
 })
 
-router.get('/message', listMessageController.hanle)
+router.get('/message', listMessageController.handle)
 router.post('/message', createMessageController.handle)
+router.post('/category', categoryController.handle)
+router.get('/categories', listCategoriesController.handle)
 
 export { router }
