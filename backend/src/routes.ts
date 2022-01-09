83451ccac35bd287b00aea34f020dad28bdc599f
Pedro <pedro-gomes17@hotmail.com>
2022-01-09 09:55:24 -0300

    chore: added create product route

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -5,6 +5,7 @@ import { ListCategoriesController } from "./controllers/ListCategoriesController
 import { CreateCategoryController } from "./controllers/CreateCategoryController";
 import { UpdateCategoryController } from "./controllers/UpdateCategoryController";
 import { DeleteCategoryController } from "./controllers/DeleteCategoryController";
+import { CreateProductController } from "./controllers/CreateProductController";
 
 const router = Router();
 
@@ -16,15 +17,21 @@ const createCategoryController = new CreateCategoryController();
 const updateCategoryController = new UpdateCategoryController();
 const deleteCategoryController = new DeleteCategoryController();
 
+const createProductController = new CreateProductController();
+
+
 router.get('/', (request: Request, response: Response) => {
     return response.json({message: 'Bem vindo a API Dio Shopping'})
 })
 
 router.get('/message', listMessageController.handle)
 router.post('/message', createMessageController.handle)
+
 router.post('/category', createCategoryController.handle)
 router.put('/category/:id', updateCategoryController.handle)
 router.delete('/category/:id', deleteCategoryController.handle)
 router.get('/categories', listCategoriesController.handle)
+
+router.post('/product', createProductController.handle)
 
 export { router }
