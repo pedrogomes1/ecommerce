d945f74f1d421a88b958ad75dd7cccad0b69eb82
Pedro <pedro-gomes17@hotmail.com>
2022-01-10 19:38:51 -0300

    chore: set process env to port

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -8,11 +8,13 @@ import "./database";
 
 const app = express();
 
+const PORT = process.env.PORT || 8080;
+
 app.use(cors());
 
 app.use(express.json());
 app.use(router)
 
-app.listen(5000, () => {
-    console.log('Server on port: 5000')
+app.listen(PORT, () => {
+    console.log(`Server on port: ${PORT}`)
 })
