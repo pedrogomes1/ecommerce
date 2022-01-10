3351e672a570a529ad0d2e256c152da88068305e
Pedro <pedro-gomes17@hotmail.com>
2022-01-09 22:53:28 -0300

    chore: removal div inline style to component

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,12 +1,14 @@
 import { Aside } from '../../components/Aside';
 import { ProductsList } from '../../components/ProductsList';
 
+import * as S from './styles';
+
 const Home = () => {
   return (
-    <div style={{ display: 'flex' }}>
+    <S.Container>
       <Aside />
       <ProductsList />
-    </div>
+    </S.Container>
   );
 };
 
---
+++ frontend/src/pages/Home/styles.ts
@@ -0,0 +1,7 @@
+import styled from '@mui/material/styles/styled';
+
+const Container = styled('div')(() => ({
+  display: 'flex',
+}));
+
+export { Container };
