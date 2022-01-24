5e28d6d23e56c31e861e589db0cc173292826b75
Pedro <pedro-gomes17@hotmail.com>
2022-01-23 21:57:05 -0300

    chore: update default collect coverage prop to true

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -2,6 +2,6 @@ export default {
   testEnvironment: 'jsdom',
   preset: 'ts-jest/presets/js-with-ts-esm',
   setupFilesAfterEnv: ['<rootDir>/src/jest-setup.ts'],
-  collectCoverage: false,
+  collectCoverage: true,
   collectCoverageFrom: ['src/**/*.ts(x)?'],
 };
