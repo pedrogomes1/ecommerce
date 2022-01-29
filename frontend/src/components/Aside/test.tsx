f3109e29d46b58fb77e27fc343987aeecd39a699
Pedro <pedro-gomes17@hotmail.com>
2022-01-29 20:05:27 -0300

    chore: added new aside testing

--- frontend/src/components/Aside/test.tsx
+++ frontend/src/components/Aside/test.tsx
@@ -1,21 +1,47 @@
 import { MemoryRouter } from 'react-router-dom';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { Aside } from '.';
 
 const mockedUsedNavigate = jest.fn();
+
+const mockedTest = jest.fn(() => Promise.resolve(true));
+
+jest.mock('axios', () => ({
+  ...jest.requireActual('axios'),
+  get: mockedTest,
+}));
 
 jest.mock('react-router-dom', () => ({
   ...jest.requireActual('react-router-dom'),
   useNavigate: () => mockedUsedNavigate,
 }));
 
+jest.mock('../../contexts/products', () => ({
+  useProducts: jest.fn(() => ({
+    fetchProducts: jest.fn(),
+  })),
+}));
+
 describe('Aside component', () => {
   it('should render category title', () => {
-    render(<Aside />, {
-      wrapper: MemoryRouter,
-    });
+    render(<Aside />, { wrapper: MemoryRouter });
     expect(
       screen.getByRole('heading', { name: /Categorias/i }),
     ).toBeInTheDocument();
+  });
+
+  it('must set checkbox to true when clickeds', async () => {
+    render(<Aside />, { wrapper: MemoryRouter });
+
+    const checkboxInputElement = await screen.findByRole('checkbox', {
+      name: /camisetas/i,
+    });
+
+    expect(checkboxInputElement).toBeInTheDocument();
+    expect(checkboxInputElement).not.toBeChecked();
+
+    fireEvent.click(checkboxInputElement);
+
+    expect(checkboxInputElement).toBeChecked();
   });
 });
