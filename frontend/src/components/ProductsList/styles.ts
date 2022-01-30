7579aea6fbd241a6aea82d14569021ba6eae32f5
Pedro <pedro-gomes17@hotmail.com>
2022-01-29 21:44:55 -0300

    chore: updated styled imports using destructuring

--- frontend/src/components/ProductsList/styles.ts
+++ frontend/src/components/ProductsList/styles.ts
@@ -1,5 +1,5 @@
 import { Card } from '@mui/material';
-import styled from '@mui/material/styles/styled';
+import { styled } from '@mui/material/styles';
 
 const Products = styled('ul')(({ theme }) => ({
   display: 'flex',
