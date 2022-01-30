6a8ac536955fe8ab4f2afaee6e2075ffee356519
Pedro <pedro-gomes17@hotmail.com>
2022-01-30 09:26:58 -0300

    test: new should update input values ​​when they change on Contact page

--- frontend/src/pages/Contact/test.tsx
+++ frontend/src/pages/Contact/test.tsx
@@ -2,11 +2,11 @@ import { render, screen, fireEvent } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import { Contact } from '.';
 
-const mockedTest = jest.fn(() => Promise.resolve(true));
+const mockedFetchMessages = jest.fn(() => Promise.resolve(true));
 
 jest.mock('axios', () => ({
   ...jest.requireActual('axios'),
-  get: mockedTest,
+  get: mockedFetchMessages,
 }));
 
 jest.mock('../../contexts/products', () => ({
@@ -22,5 +22,20 @@ describe('Contact page', () => {
     expect(screen.getByLabelText(/E-mail/i)).toBeInTheDocument();
     expect(screen.getByLabelText(/Mensagem/i)).toBeInTheDocument();
     expect(screen.getByRole('button', { name: /Enviar/i })).toBeInTheDocument();
+  });
+
+  it('should update input values ​​when they change', async () => {
+    render(<Contact />, { wrapper: MemoryRouter });
+
+    const emailInput = screen.getByLabelText(/E-mail/i) as HTMLInputElement;
+    const messageInput = screen.getByLabelText(/Mensagem/i) as HTMLInputElement;
+
+    fireEvent.change(emailInput, {
+      target: { value: 'johndoe@example.com' },
+    });
+    fireEvent.change(messageInput, { target: { value: 'Hello World' } });
+
+    expect(emailInput.value).toBe('johndoe@example.com');
+    expect(messageInput.value).toBe('Hello World');
   });
 });
