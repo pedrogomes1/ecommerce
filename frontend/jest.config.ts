54c4ba0f4c3936f54a904065ae36716841892247
Pedro <pedro-gomes17@hotmail.com>
2022-02-17 15:34:25 -0300

    chore: update path for setupFilesAfterEnv variable

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -2,7 +2,7 @@ export default {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/'],
   preset: 'ts-jest/presets/js-with-ts-esm',
-  setupFilesAfterEnv: ['<rootDir>/src/tests/setupTests.ts'],
+  setupFilesAfterEnv: ['<rootDir>/src/jest-setup.ts'],
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.ts(x)?'],
 };
